654ec517e1dd653d2405eb90d8b8725aaa55c4f0
duanexavierbondad <dxbondad36@gmail.com>
2026-05-04 16:53:19 +0800

    bug fixes

--- boards/shields/nice_view_xavient/widgets/peripheral_status.c
+++ boards/shields/nice_view_xavient/widgets/peripheral_status.c
@@ -8,14 +8,6 @@
 #include <zephyr/kernel.h>
 #include <zephyr/random/random.h>
 
-LV_IMG_DECLARE(left);
-LV_IMG_DECLARE(right);
-
-const lv_img_dsc_t *anim_imgs[] = {
-    &left,
-    &right,
-};
-
 
 
 
