0b37eaa7dc6d0ccd5582f1d679f19b2401c7b13d
duanexavierbondad <dxbondad36@gmail.com>
2026-05-04 15:53:10 +0800

    implemented lgvl 9

--- boards/shields/nice_view_xavient/widgets/right.c
+++ boards/shields/nice_view_xavient/widgets/right.c
@@ -117,9 +117,8 @@ const LV_ATTRIBUTE_MEM_ALIGN LV_ATTRIBUTE_LARGE_CONST LV_ATTRIBUTE_IMG_RIGHT uin
 };
 
 const lv_img_dsc_t right = {
-    .header.cf = LV_IMG_CF_INDEXED_1BIT,
-    .header.always_zero = 0,
-    .header.reserved = 0,
+    .header.cf = LV_COLOR_FORMAT_I1,
+
     .header.w = 140,
     .header.h = 68,
     .data_size = sizeof(right_map),
